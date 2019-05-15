e587b73ee8feee2526380cc762bf90fd8b37a824
Rafael Duque <rafaelduqueestrada@gmail.com>
2019-05-15 19:41:39 -0300

    Including i18n

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldRestController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldRestController.java
@@ -1,10 +1,18 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldRestController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -13,6 +21,7 @@ public class HelloWorldRestController {
 
     @GetMapping("hello-world-bean")
     public HelloWorldBean helloWorldWithBean() {
-        return new HelloWorldBean("Hello World noob!");
+        Locale locale = LocaleContextHolder.getLocale();
+        return new HelloWorldBean(messageSource.getMessage("good.morning.message", null, locale));
     }
 }
